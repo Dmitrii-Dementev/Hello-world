53b83a1ea400e21bf1d1618605b9a313821cf026
Dmitrii Dementev <dima.dem.2015@mail.ru>
2020-11-06 18:48:59 +0300

    Start of design

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");
+
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("Sum = " + c);
+
+        int a = 1;
+        int b = 2;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b)); */
+
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
+
+    }
+}
